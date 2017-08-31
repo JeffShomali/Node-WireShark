d105bb3c9925c79f78c9d423fc045deb6c6afc43
Jeff Shomali <jeffshomali@gmail.com>
2017-08-30 19:50:18 -0700

    Using ip-api.com API to fetch data

--- src/GetIPAdress.js
+++ src/GetIPAdress.js
@@ -1,37 +1,33 @@
 const request = require('request');
 
 
- const getPublicIP = () => {
+const getPublicIP = () => {
     let exec = require('child_process').execSync;
-    let cmd = "dig +short myip.opendns.com @resolver1.opendns.com"; 
+    let cmd = "dig +short myip.opendns.com @resolver1.opendns.com";
     let options = {
-      encoding: 'utf8'
+        encoding: 'utf8'
     };
-    
+
     return exec(cmd, options);
- }
+}
 
- var ip = getPublicIP();
+var ip = getPublicIP();
 
-const showMyPublicIPInfo = (_ip) => {
+const showMyPublicIPInfo = (_ip, callback) => {
+    let IPInformation = [];
     request({
         url: `http://ip-api.com/json/${_ip}`,
-         }, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-               console.log(body.as);
-            } else {
-                console.log('Unable to fetch the weather')
-            }
-      });
-} 
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+           IPInformation.push(body.city);
 
-showMyPublicIPInfo(ip);
+        } else {
+            callback('Unable to fetch you IP address')
+        }
+        return IPInformation;
+    });
+}
 
-
-// function (error, response, body) {
-//     console.log('error:', error); 
-//     console.log('statusCode:', response && response.statusCode); 
-//     console.log('body:', body); 
-
-
-module.exports.showMyPublicIPInfo = showMyPublicIPInfo;
+var result = showMyPublicIPInfo(ip);
+// console.log(result);
